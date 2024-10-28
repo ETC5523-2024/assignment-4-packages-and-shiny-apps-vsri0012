4b7c4f0151c92847747106a28546ecabefe7e587
Varun Sridhar <vsri0012@student.monash.edu>
2024-10-29 03:25:44 +1100

    final commit

--- R/launch_shiny_app.R
+++ R/launch_shiny_app.R
@@ -1,17 +1,19 @@
-# R/launch_shiny_app.R
-
-#' Launch the WWEAnalytics Shiny App
+#' Launch the Shiny App from the inst Folder
 #'
-#' This function launches the Shiny app for exploring WWE Championship reign data.
-#' It assumes that shiny-app.R is located in the 'inst' folder of the package.
+#' This function launches a Shiny application located in the inst folder of the package.
+#' @param launch.browser Logical; if TRUE, opens the app in the default web browser.
 #'
+#' @return No return value; the function is called for its side effect of launching the app.
+#' @examples
+#' launch_shiny_app()
 #' @export
-launch_shiny_app <- function() {
-  app_file <- system.file("shiny-app.R", package = "WWEAnalytics")
+launch_shiny_app <- function(launch.browser = TRUE) {
+  app_directory <- system.file("shiny-app.R", package = "WWEAnalytics")
 
-  if (app_file == "") {
-    stop("Could not find shiny-app.R file. Please ensure shiny-app.R is in the 'inst' folder of the WWEAnalytics package.")
+  # Check if the app file exists
+  if (!file.exists(app_directory)) {
+    stop("The Shiny app file does not exist in the inst folder.")
   }
-
-  shiny::runApp(app_file)
+  # Launch the Shiny app
+  shiny::runApp(app_directory, launch.browser = launch.browser)
 }
